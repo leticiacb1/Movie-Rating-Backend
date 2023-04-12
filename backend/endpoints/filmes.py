779cd5d9d0fb242141f2b08a774289d2f3488144
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-04-12 09:06:56 -0300

    Sobe documentacao do filme.

--- backend/endpoints/filmes.py
+++ backend/endpoints/filmes.py
@@ -11,6 +11,42 @@ router = APIRouter()
 async def get_all_filmes():
 
     """
+    Retorna as informações de todos os filmes da base no formato:
+
+    **filme_id** : {\\
+            name : "Titanic",\\
+            description : " Um filme que mistura um jovem romance com uma infeliz tragédia.",\\
+            release_year 1998,\\
+            length : 125\\
+    }
+
+    """
+
+    result = get_all_films()
+    return {"status_code": 200 , "filmes" : result}
+
+@router.get("/filmes/{id}" , summary="Retorna filme do id especificado")
+async def get_filme_id(id: int):
+
+    """
+    Retorna as informação do filme com o respectivo {id}, no formato:
+
+    **filme_id** : {\\
+        name : "Titanic",\\
+        description : " Um filme que mistura um jovem romance com uma infeliz tragédia.",\\
+        release_year: 1998,\\
+        length : 125\\
+    }
+
+    """
+
+    result = get_film(id)
+    return {"status_code": 200 , "filme" : result}
+
+@router.post("/filmes/", summary="Cadastra um novo filme")
+async def cadastra_filmes(film: Filme):
+
+    """
     Cria um filme com as seguintes informações
 
     - **name**: Título do filme
@@ -20,16 +56,6 @@ async def get_all_filmes():
     
     """
 
-    result = get_all_films()
-    return {"status_code": 200 , "filmes" : result}
-
-@router.get("/filmes/{id}")
-async def get_filme_id(id: int):
-    result = get_film(id)
-    return {"status_code": 200 , "filme" : result}
-
-@router.post("/filmes/")
-async def cadastra_filmes(film: Filme):
     result = create_film(film)
 
     if(result == "OK"):
@@ -37,8 +63,19 @@ async def cadastra_filmes(film: Filme):
     else:
         return {"status_code": 404 , "mensagem":result }  
 
-@router.put("/filmes/{id}/")
+@router.put("/filmes/{id}/" , summary="Atualiza a informação de um filme")
 async def update_filme(id: int , film: Filme):
+
+    """
+    Atualiza o filme de {id} correspondente, com possibilidade de mudança nos seguintes campos:
+
+    - **name**: Título do filme
+    - **description** : breve sinopse do filme
+    - **release_year** : ano de lançamento do filme
+    - **length** : duração do filme em minutos
+    
+    """
+
     result = update_film(id , film.dict())
 
     if(result == "OK"):
@@ -46,7 +83,7 @@ async def update_filme(id: int , film: Filme):
     else:
         return {"status_code": 404 , "mensagem":result }
 
-@router.delete("/filmes/{id}")
-async def deleta_filme(id: int):
+@router.delete("/filmes/{id}", summary="Deleta o filme do {id} especificado")
+async def deleta_filme(id: int): 
     result = delete_film(id)
     return {"status_code": 200 } 
