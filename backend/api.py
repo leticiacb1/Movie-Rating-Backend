b4edb3798d9ad01e72559e5cfb6ade9868d72c72
Adneycm <adneycm@al.insper.edu.br>
2023-04-28 19:04:11 -0300

    alterações de caminhos de pastas

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,12 @@
-print("teste")
+from fastapi import APIRouter
+
+from backend.routers import filmes , avaliacoes
+from backend.enums.tags import Tags
+
+api = APIRouter(prefix='/api')
+
+# Filmes
+api.include_router(filmes.router , tags = [Tags.filmes])
+
+# Avaliacoes
+api.include_router(avaliacoes.router , tags = [Tags.avaliacoes])
